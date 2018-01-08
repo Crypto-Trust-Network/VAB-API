0c7e51158ff02ad7b0f49747e1ba833d2a6644a9
Allan Walter <allan@digitalstock.co.nz>
2018-01-08 15:31:11 +1300

    Add project files.

---
+++ CTN-VAB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CTN_VAB.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CTN-VAB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CTN_VAB.WebApiApplication" Language="C#" %>
